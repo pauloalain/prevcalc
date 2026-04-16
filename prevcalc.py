f32aef8f9542c39014a28468bdef32270a8d5e7b
Paulo Querette <pauloalain@gmail.com>
2026-04-16 18:48:16 -0300

    Ajustar Redutor Deficiencias

--- prevcalc.py
+++ prevcalc.py
@@ -14,7 +14,7 @@ class Aposentadoria(ABC):
         self.idade = int(kwargs.get('idade', 0))
         self.tempo_contribuicao = int(kwargs.get('tempo_contribuicao', 0))
         self.tempo_servico_publico = int(kwargs.get('tempo_servico_publico', 0))
-        self.tempo_cargo = int(kwargs.get('tempo_cargo'))
+        self.tempo_cargo = int(kwargs.get('tempo_cargo', 0))
         self.dados_extras = kwargs
 
     @classmethod
@@ -57,6 +57,7 @@ class Especial(Aposentadoria):
         @classmethod      
         
         def checar_tipo(cls,**kwargs):
+            especial = kwargs.get('especial', '').upper()
             sexo = kwargs.get('sexo', '').upper()
             idade = int(kwargs.get('idade', 0))
             tempo_contribuicao = int(kwargs.get('tempo_contribuicao', 0))
@@ -69,19 +70,21 @@ class Especial(Aposentadoria):
                 idade_minima = 57
             elif sexo.upper() != 'M':
                 return False
-            return (idade >= idade_minima and 
+            return (especial == 'S' and idade >= idade_minima and 
                     tempo_contribuicao >= cls.min_tempo_contribuicao and 
                     tempo_servico_publico >= cls.min_servico_publico and 
                     tempo_cargo >= cls.min_cargo and
                     tempo_exposicao >= 25)
         
 class Deficiencia(Aposentadoria):
+        redutor_especifico = 0
         def __init__(self,  **kwargs):
             super().__init__(**kwargs)
             
         @classmethod      
         
         def checar_tipo(cls,**kwargs):
+            deficiencia = kwargs.get('deficiencia', '').upper()
             sexo = kwargs.get('sexo', '').upper()
             idade = int(kwargs.get('idade', 0))
             tempo_contribuicao = int(kwargs.get('tempo_contribuicao', 0))
@@ -89,21 +92,46 @@ class Deficiencia(Aposentadoria):
             tempo_cargo = int(kwargs.get('tempo_cargo', 0))
 
 # TODO: CRIAR A VARIÁVEL DEFICIENCIA, FAZER AS SUBCLASSES
-
-
-            grau_deficiencia = str(kwargs.get('grau_deficiencia', '').upper())
-               
-               
-            idade_minima = 60
-            if sexo == 'F':
-                idade_minima = 55
-            elif sexo != 'M':
-                return False
+            grau_deficiencia_raw = kwargs.get('grau_deficiencia', '')
+            grau_informado = str(grau_deficiencia_raw or "").strip().upper()
             
-            return (grau_deficiencia == cls.__name__.replace('Deficiencia', '').upper() and idade >= idade_minima and 
+            if grau_informado in ("0", "NONE"):
+                grau_informado = ""
+                
+            graudaclasse = cls.__name__.replace('Deficiencia', '').upper()
+            
+            if graudaclasse == "":
+                valida_grau = True
+                idade_minima = 60 if sexo == 'M' else 55
+            else:
+                valida_grau = (grau_informado == graudaclasse)
+                idade_minima = 0
+                
+            if sexo not in ['M', 'F']:
+                return False
+                        
+            return (deficiencia == 'S' and valida_grau and idade >= idade_minima and 
                     tempo_contribuicao >= cls.min_tempo_contribuicao - 10 and 
                     tempo_servico_publico >= cls.min_servico_publico and 
                     tempo_cargo >= cls.min_cargo)
             
 class DeficienciaGrave(Deficiencia):
-    raise NotImplementedError
+        @classmethod      
+
+        def checar_tipo(cls,**kwargs):
+            deficiencia = kwargs.get('deficiencia', '').upper()
+            sexo = kwargs.get('sexo', '').upper()
+            tempo_contribuicao = int(kwargs.get('tempo_contribuicao', 0))
+            tempo_servico_publico = int(kwargs.get('tempo_servico_publico', 0))
+            tempo_cargo = int(kwargs.get('tempo_cargo', 0))
+            
+            redutor = 5 if sexo =='F' else 0
+             
+            return deficiencia == S and tempo_contribuicao >= cls.min_tempo_contribuicao - redutor and tempo_servico_publico >= cls.min_servico_publico and tempo_cargo >= cls.min_cargo
+    
+
+class DeficienciaModerada(Deficiencia):
+    pass
+
+class DeficienciaLeve(Deficiencia):
+    pass
